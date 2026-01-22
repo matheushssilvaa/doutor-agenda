81f79cefd38ba9bbb20c85b30b723ea753883a76
matheushssilvaa <msxatendimento@outlook.com.br>
2026-01-22 13:29:40 -0300

    feat: ensure doctor availability is saved as utc on database

--- src/app/actions/upsert-doctor/index.ts
+++ src/app/actions/upsert-doctor/index.ts
@@ -6,9 +6,30 @@ import { doctorsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { actionClient } from "@/lib/next-safe-action";
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+
+dayjs.extend(utc)
 
 export const upsertDoctor = actionClient.schema(upsertDoctorSchema)
 	.action(async ({ parsedInput }) => {
+
+		// conversão de datas para UTC
+		const availableFromTime = parsedInput.availableFromTime
+		const availableToTime = parsedInput.availableToTime
+
+		const availableFromTimeUTC = dayjs()
+			.set("hour", parseInt(availableFromTime.split(":")[0]))
+			.set("minute", parseInt(availableFromTime.split(":")[1]))
+			.set("second", parseInt(availableFromTime.split(":")[2]))
+			.utc()
+
+		const availableToTimeUTC = dayjs()
+			.set("hour", parseInt(availableToTime.split(":")[0]))
+			.set("minute", parseInt(availableToTime.split(":")[1]))
+			.set("second", parseInt(availableToTime.split(":")[2]))
+			.utc()
+
 		const session = await auth.api.getSession({
 			headers: await headers()
 		})
@@ -24,14 +45,18 @@ export const upsertDoctor = actionClient.schema(upsertDoctorSchema)
 		await db.
 			insert(doctorsTable)
 			.values({
+				...parsedInput,
 				id: parsedInput.id,
 				clinicId: session?.user.clinic?.id,
-				...parsedInput,
+				availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+				availableToTime: availableToTimeUTC.format("HH:mm:ss")
 			})
 			.onConflictDoUpdate({
 				target: [doctorsTable.id],
 				set: {
-					...parsedInput
+					...parsedInput,
+					availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+					availableToTime: availableToTimeUTC.format("HH:mm:ss")
 				}
 			})
 	})
